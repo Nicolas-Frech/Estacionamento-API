a653722519d34935e13fddfe8843c3f43084803e
Nicolas Frech <nicolasfrechdev1@gmail.com>
2024-10-25 11:26:37 -0300

    Add VehicleControllerTest

--- src/test/java/br/com/nicolas_frech/garagem_API/controller/VehicleControllerTest.java
+++ src/test/java/br/com/nicolas_frech/garagem_API/controller/VehicleControllerTest.java
@@ -1,6 +1,6 @@
 package br.com.nicolas_frech.garagem_API.controller;
 
-import br.com.nicolas_frech.garagem_API.service.ParkingService;
+import br.com.nicolas_frech.garagem_API.repository.VehicleRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,66 +13,56 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-@SpringBootTest
 @AutoConfigureMockMvc
-class ParkingControllerTest {
+@SpringBootTest
+class VehicleControllerTest {
 
     @Autowired
-    private MockMvc mvc;
+    MockMvc mvc;
 
     @MockBean
-    private ParkingService parkingService;
+    VehicleRepository vehicleRepository;
 
     @Test
-    @DisplayName("Deve retornar código 400 para registro de estacionamento com erros")
+    @DisplayName("Deveria retornar código 400 para registro de veículo com erros")
     void scenary01() throws Exception {
         //ARRANGE
         String json = "{}";
 
         //ACT
         var response = mvc.perform(
-                post("/estacionamento")
+                post("/veiculos")
                         .content(json)
                         .contentType(MediaType.APPLICATION_JSON)
         ).andReturn().getResponse();
 
-
         //ASSERT
+
         assertEquals(400, response.getStatus());
     }
 
     @Test
-    @DisplayName("Deve retornar código 200 para registro de estacionamento sem erros")
+    @DisplayName("Deveria retornar código 200 para registro de veiculo sem erros")
     void scenary02() throws Exception {
         //ARRANGE
         String json = """
                 {
-                "name" : "Estacionamento",
-               	"cnpj" : "81.474.316/0001-17",
-               	"address" : {
-               		"street" : "Rua das Flores",
-               		"district" : "Vila Velha",
-               		"cep" : "88345432",
-               		"city" : "Curitiba",
-               		"state" : "Paraná",
-               		"number" : "222"
-               	    },
-               	"phone" : "47993456829",
-               	"bikeSpace" : "10",
-               	"carSpace" : "15"
-               	}
+                	"brand" : "veiculo",
+                	"model" : "XM",
+                	"color" : "Vermelho",
+                	"plate" : "2345642",
+                	"type" : "CAR"
+                }
                 """;
 
         //ACT
         var response = mvc.perform(
-                post("/estacionamento")
+                post("/veiculos")
                         .content(json)
                         .contentType(MediaType.APPLICATION_JSON)
         ).andReturn().getResponse();
 
-
         //ASSERT
         assertEquals(200, response.getStatus());
     }
-
 }
